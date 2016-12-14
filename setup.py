6776f978a1cdd33edcf0ec47117768fdf49d13e4
Roman Yurchak <rth.yurchak@gmail.com>
2016-12-14 12:54:21 +0100

    Version 0.94

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 kw = {
     'name': 'jwzthreading',
-    'version': '0.93',
+    'version': '0.94',
     'description': 'Algorithm for threading mail messages.',
     'long_description' : '''Contains an implementation of an algorithm for threading mail
 messages, as described at http://www.jwz.org/doc/threading.html.''',
